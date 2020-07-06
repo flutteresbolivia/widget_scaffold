89dc30e737c7b574b58a6d66c4288608c629fdf3
waperp <miguelhangelh@hotmail.com>
2020-07-06 11:59:47 -0400

    seccond

--- lib/main.dart
+++ lib/main.dart
@@ -8,15 +8,19 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         backgroundColor: Colors.white,
         appBar: AppBar(
+          centerTitle: true,
+          title: Text("AppBar"),
           backgroundColor: Colors.purple,
         ),
         floatingActionButton: FloatingActionButton(
           onPressed: () {},
-          backgroundColor: Colors.indigo,
+          backgroundColor: Colors.deepPurple,
         ),
+        body: Center(child: Text("SCAFFOLD", style: TextStyle(fontSize: 40))),
       ),
     );
   }
